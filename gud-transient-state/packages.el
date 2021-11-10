33214ac6b421da761c4c0bbae84c86927a38d3e8
limoiie <limo.iie4@gmail.com>
2021-11-10 03:06:02 +0000

    refactor: Bind gud-transient-state to ,d.

--- gud-transient-state/packages.el
+++ gud-transient-state/packages.el
@@ -80,8 +80,9 @@ Breakpoints^^     Execution^^^^            Eval^^                   View^^
         ("pe" gud-pp)
         )
 
-      ;; TODO: hook on Mode debugger, Buffer *gud-main*
       (spacemacs/set-leader-keys "md." 'spacemacs/gud-transient-state/body)
+      (spacemacs/set-leader-keys-for-minor-mode 'lsp-mode "d."
+        'spacemacs/gud-transient-state/body)
       (defadvice gud-display-line (after gud-display-line-centered activate)
         "Center the line in the window"
         (when (and gud-overlay-arrow-position gdb-source-window)
